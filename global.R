070b42207a9a1872bf94c3b013185f80cca3b240
majpark21 <marc-antoine.jacques@izb.unibe.ch>
2019-10-16 15:57:57 +0200

    Moved automatic installation of packages into global.R

--- global.R
+++ global.R
@@ -1,3 +1,11 @@
+# Check if all required packages are installed, if not attempt to install the missing ones
+required_packages = c("shiny", "shinyjs", "shinyBS", "shinycssloaders",
+                      "data.table", "DT", "ggplot2", "gplots", "plotly",
+                      "scales", "grid", "dendextend", "RColorBrewer",
+                      "ggthemes", "sparcl", "dtw", "factoextra", "imputeTS",
+                      "MASS", "robust", "pracma", "Hmisc")
+missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+if(length(missing_packages)) install.packages(missing_packages)
 source('modules/auxfunc.R')
 source('modules/selOutliers.R')
 source('modules/downPlot.R')
