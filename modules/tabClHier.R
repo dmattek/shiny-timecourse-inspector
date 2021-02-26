bf13460a728a8e7ef5d58019a57b180a85dd5de3
dmattek <macdobry@gmail.com>
2021-02-26 12:14:34 +0100

    Added PCA tab

--- modules/tabClHier.R
+++ modules/tabClHier.R
@@ -5,7 +5,7 @@
 # This module is a tab for hierarchical clustering (base R hclust + dist)
 
 helpText.clHier = c(alertNAsPresentClDTW = paste0("NAs (still) present. DTW cannot calculate the distance. ",
-                                                "If interpolation is active in the left panel, missing data can be due to removed outlier time points."),
+                                                  "If interpolation is active in the left panel, missing data can be due to removed outlier time points."),
                     alertNAsPresentCl = paste0("NAs (still) present, caution recommended. If interpolation is active in the left panel, ",
                                                "missing data can be due to removed outlier time points."),
                     alertNAsInDist = "NAs in distance matrix. Possible non-overlapping time series in the dataset.",
@@ -105,6 +105,14 @@ clustHierUI <- function(id, label = "Hierarchical Clustering") {
                        placement = "top",
                        trigger = "hover",
                        options = NULL)
+      ),
+      column(3,
+             selectInput(
+               ns("selectPlotHierPaletteDend"),
+               label = "Dendrogram\'s colour palette",
+               choices = l.col.pal.dend.2,
+               selected = 'Color Blind'
+             )
       )
     ),
     
@@ -135,13 +143,6 @@ clustHierUI <- function(id, label = "Hierarchical Clustering") {
                         )
                  ),
                  column(3,
-                        selectInput(
-                          ns("selectPlotHierPaletteDend"),
-                          label = "Dendrogram\'s colour palette",
-                          choices = l.col.pal.dend.2,
-                          selected = 'Color Blind'
-                        ),
-                        checkboxInput(ns('selectPlotHierDend'), 'Plot dendrogram and re-order samples', TRUE),
                         sliderInput(
                           ns('inPlotHierNAcolor'),
                           'Shade of grey for NA values',
@@ -150,8 +151,8 @@ clustHierUI <- function(id, label = "Hierarchical Clustering") {
                           value = 0.8,
                           step = .1,
                           ticks = TRUE
-                        )
-                        
+                        ),
+                        checkboxInput(ns('selectPlotHierDend'), 'Plot dendrogram and re-order samples', TRUE)
                  ),
                  column(3,
                         numericInput(
@@ -215,8 +216,11 @@ clustHierUI <- function(id, label = "Hierarchical Clustering") {
       
       tabPanel('Cluster distribution',
                br(),
-               modClDistPlotUI(ns('hierClDistPlot'), 'xxx'))
+               modClDistPlotUI(ns('hierClDistPlot'), 'xxx')),
       
+      tabPanel('PCA',
+               br(),
+               modPCAplotUI(ns('modPlotHierPCA')))
     )
   )
 }
@@ -226,6 +230,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   
   ns <- session$ns
   
+  ## UI ----
   # Return the number of clusters from the slider 
   # and delay by a constant in milliseconds defined in auxfunc.R
   returnNclust = reactive({
@@ -234,7 +239,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   
   # not functional; see th note in UI
   output$uiPlotHierClAss = renderUI({
-
+    
     if(input$chBPlotHierClAss) {
       selectInput(ns('inPlotHierClAss'), 'Assign cluster order', 
                   choices = seq(1, returnNclust(), 1),
@@ -244,8 +249,8 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   })
   
   output$uiPlotHierClSel = renderUI({
-
-      if(input$chBPlotHierClSel) {
+    
+    if(input$chBPlotHierClSel) {
       selectInput(ns('inPlotHierClSel'), 'Select clusters to display', 
                   choices = seq(1, returnNclust(), 1),
                   multiple = TRUE, 
@@ -257,7 +262,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   
   # UI for setting lower and upper bounds for heat map colour scale  
   output$uiSetColBoundsLow = renderUI({
-
+    
     if(input$chBsetColBounds) {
       
       loc.dt = in.dataLong()
@@ -275,7 +280,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   
   
   output$uiSetColBoundsHigh = renderUI({
-
+    
     if(input$chBsetColBounds) {
       
       loc.dt = in.dataLong()
@@ -291,11 +296,12 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     }
   })
   
+  ## Processing ----
   
   # calculate distance matrix for further clustering
   # time series arranged in rows with columns corresponding to time points
-  userFitDistHier <- reactive({
-    cat(file = stderr(), 'userFitDistHier \n')
+  calcDist <- reactive({
+    cat(file = stderr(), 'calcDist \n')
     
     loc.dm = in.dataWide()
     
@@ -347,25 +353,37 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
       return(cl.dist)
     }
     
-
+    
   })
   
-  # perform hierarchical clustering and return dendrogram coloured according to cutree
-  # branch coloring performed using dendextend package
-  userFitDendHier <- reactive({
-    cat(file = stderr(), 'userFitDendHier \n')
+  # perform hierarchical clustering 
+  calcHclust <- reactive({
+    cat(file = stderr(), 'calcHclust \n')
     
     # calculate distance matrix
-    loc.dm.dist = userFitDistHier()
+    loc.dm.dist = calcDist()
     
     if (is.null(loc.dm.dist)) {
       return(NULL)
     }
     
-    #print(sum(is.na(loc.dm.dist)))
-    
     loc.cl.hc = hclust(loc.dm.dist, method = input$selectPlotHierLinkage)
     
+    return(loc.cl.hc)
+  })
+  
+  # return dendrogram coloured according to cutree
+  # branch coloring performed using dendextend package
+  calcDendCutColor <- reactive({
+    cat(file = stderr(), 'calcDendCutColor \n')
+    
+    # calculate distance matrix
+    loc.cl.hc = calcHclust()
+    
+    if (is.null(loc.cl.hc)) {
+      return(NULL)
+    }
+
     # number of clusters at which dendrogram is cut
     loc.k = returnNclust()
     
@@ -395,36 +413,31 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   getClColHier <- reactive({
     cat(file = stderr(), 'getClColHier \n')
     
-    loc.dend = userFitDendHier()
+    loc.dend = calcDendCutColor()
     if (is.null(loc.dend))
       return(NULL)
-    
+
     # obtain relations between cluster and colors from the dendrogram
     loc.dt = LOCgetClCol(loc.dend, returnNclust())
-
+    
     # Display clusters specified in the inPlotHierClSel field
-    # Data is ordered according to the order of clusters specified in this field
+    # Clusters are ordered according to the order of clusters specified in this field
     if(input$chBPlotHierClSel) {
       # keep only clusters specified in input$inPlotHierClSel
       loc.dt = loc.dt[gr.no %in% input$inPlotHierClSel]
       loc.dt[, gr.no := factor(gr.no, levels = input$inPlotHierClSel)]
     }
 
-    # sort by cluster number
-    setorder(loc.dt, gr.no)
-    
     # set the key to allow sub-setting
     setkey(loc.dt, gr.no)
-# 
-#     cat("tabClHier::getClColHier::locdt\n")
-#     print(loc.dt)
-#     cat("\n\n")
     
     return(loc.dt)
   })
   
   
   
+  
+  # UNUSED ATM
   # Return all unique track object labels (created in dataMod)
   # This will be used to display in UI for trajectory highlighting
   getDataTrackObjLabUni_afterTrim <- reactive({
@@ -450,6 +463,39 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     
   })
   
+  
+  
+  # Prepare data with track IDs and cluster numbers
+  # Used for colouring points by cluster numbers in PCA
+  # If manual cluster selection switched on, 
+  # return only those clusters specified in the inPlotHierClSel field
+  dataIDwithCl <- reactive({
+    cat(file = stderr(), 'dataIDwithCl: in\n')
+    
+    # get cellIDs with cluster assignments based on dendrogram cut
+    loc.dt.cl = getDataCl(calcDendCutColor(), returnNclust())
+    
+    
+    if (is.null(loc.dt.cl)) {
+      cat(file = stderr(), 'dataClWithCol: dt is NULL\n')
+      return(NULL)
+    }
+    
+    cat(file = stderr(), 'dataClWithCol: dt not NULL\n')
+
+    # Display clusters specified in the inPlotHierClSel field
+    # Data is ordered according to the order of clusters specified in this field
+    if(input$chBPlotHierClSel) {
+      loc.dt.cl = loc.dt.cl[get(COLCL) %in% input$inPlotHierClSel]
+      loc.dt.cl[, (COLCL) := factor(get(COLCL), levels = input$inPlotHierClSel)]
+      setkeyv(loc.dt.cl, COLCL)
+    } else {
+      loc.dt.cl[, (COLCL) := as.factor(get(COLCL))]
+    }
+    
+    return(loc.dt.cl)    
+  })
+  
   # prepare data for plotting trajectories per cluster
   # outputs dt as data4trajPlot but with an additional column 'cl' that holds cluster numbers
   # additionally some clusters are omitted according to manual selection
@@ -466,7 +512,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     cat(file = stderr(), 'data4trajPlotCl: dt not NULL\n')
     
     # get cellIDs with cluster assignments based on dendrogram cut
-    loc.dt.cl = getDataCl(userFitDendHier(), returnNclust())
+    loc.dt.cl = getDataCl(calcDendCutColor(), returnNclust())
     
     # add the column with cluster assignemnt to the main dataset
     loc.dt = merge(loc.dt, loc.dt.cl, by = COLID)
@@ -474,9 +520,9 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     # Display clusters specified in the inPlotHierClSel field
     # Data is ordered according to the order of clusters specified in this field
     if(input$chBPlotHierClSel) {
-      loc.dt = loc.dt[cl %in% input$inPlotHierClSel]
-      loc.dt[, cl := factor(cl, levels = input$inPlotHierClSel)]
-      setkey(loc.dt, cl)
+      loc.dt = loc.dt[get(COLCL) %in% input$inPlotHierClSel]
+      loc.dt[, (COLCL) := factor(get(COLCL), levels = input$inPlotHierClSel)]
+      setkeyv(loc.dt, COLCL)
     }
     
     return(loc.dt)    
@@ -506,7 +552,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     },
     
     content = function(file) {
-      write.csv(x = getDataCl(userFitDendHier(), returnNclust()), file = file, row.names = FALSE)
+      write.csv(x = getDataCl(calcDendCutColor(), returnNclust()), file = file, row.names = FALSE)
     }
   )
   
@@ -520,7 +566,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     },
     
     content = function(file) {
-      saveRDS(object = userFitDendHier(), file = file)
+      saveRDS(object = calcDendCutColor(), file = file)
     }
   )
   
@@ -529,7 +575,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     cat(file = stderr(), 'data4clDistPlot: in\n')
     
     # get cell IDs with cluster assignments depending on dendrogram cut
-    loc.dend <- userFitDendHier()
+    loc.dend <- calcDendCutColor()
     if (is.null(loc.dend)) {
       cat(file = stderr(), 'plotClDist: loc.dend is NULL\n')
       return(NULL)
@@ -567,6 +613,8 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     
   })
   
+  ## Plot ----
+  
   # Function instead of reactive as per:
   # http://stackoverflow.com/questions/26764481/downloading-png-from-shiny-r
   # This function is used to plot and to downoad a pdf
@@ -580,7 +628,7 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
     # Thanks to isolate all mods in the left panel are delayed 
     # until clicking the Plot button
     loc.dm = shiny::isolate(in.dataWide())
-    loc.dend = shiny::isolate(userFitDendHier())
+    loc.dend = shiny::isolate(calcDendCutColor())
     
     shiny::validate(
       shiny::need(!is.null(loc.dm), "Nothing to plot. Load data first!"),
@@ -678,6 +726,17 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
   })
   
   
+  createFnamePCAplot = reactive({
+    
+    paste0('clust_hier_tCoursesPCA_',
+           input$selectPlotHierDiss,
+           '_',
+           input$selectPlotHierLinkage, 
+           '.pdf')
+  })
+  
+  ## Modules ----
+  
   #  Hierarchical - Heat Map - download pdf
   callModule(downPlot, "downPlotHier", createFnameHeatMap, plotHier)
   
@@ -709,8 +768,15 @@ clustHier <- function(input, output, session, in.dataWide, in.dataLong, in.dataS
              in.data = data4clDistPlot,
              in.colors = getClColHier,
              in.fname = createFnameDistPlot)
-
-    # Pop-overs ----
+  
+  # plot cluster PCA
+  callModule(modPCAplot, 'modPlotHierPCA',
+             in.dataWide = in.dataWide,
+             in.idWithCl = dataIDwithCl,
+             in.clWithCol = getClColHier,
+             in.fname = createFnamePCAplot)
+  
+  # Pop-overs ----
   addPopover(session, 
              ns("alLearnMore"),
              title = "Hierarchical clustering",
